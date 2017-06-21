07633d30410cda6f9a85f891f8c6cea2d9f13f41
Mitchell Sotto <msotto@uticorp.com>
2017-06-20 17:19:54 -0700

    got background and content.js to communicate and put divs on screen with dummy data. Changed extension to not have a popup.

--- background.js
+++ background.js
@@ -11,19 +11,22 @@ var qaData = {
   }
 };
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    var msg = request.message;
+chrome.browserAction.onClicked.addListener(initializeQaTool);
 
-    if (!msg) {
-      throw "Message received, but there was no message attribute!";
-    }
-    else {
-      if (msg === 'initialize') {
-        initializeQaTool();
-      }
-    }
-  });
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     var msg = request.message;
+
+//     if (!msg) {
+//       throw "Message received, but there was no message attribute!";
+//     }
+//     else {
+//       if (msg === 'initialize') {
+//         initializeQaTool();
+//       }
+//     }
+//   });
 
 
 
@@ -97,30 +100,98 @@ chrome.runtime.onMessage.addListener(
 // not yet been activated. it also sets up the attributes of the global
 // 'qaData' object to be able to reference in other parts of the code
 function initializeQaTool() {
-  qaToolIsActive = true;
-
-  chrome.windows.create({ 
-    url: [
-      'http://avondale-iol/AD-105.html',
-      'https://prdtfs.uticorp.com/UTI-ALM/IT/BMS/_backlogs?level=Projects&showParents=false&_a=backlog',
-      'https://uti.blackboard.com/webapps/login/?action=relogin'
-    ]
-  },
-
-  // callback after the window is created
-  function(win) {
-    qaData.window = win;
-
-    // cycle through the tabs in the new window
-    chrome.tabs.query({ windowId : qaData.window.id },
-    function (tabs) {
-      for (var i = 0; i < tabs.length; i++) {
-        var curURL = tabs[i].url;
-
-        if (curURL.includes('avondale-iol'))            { qaData.tabs.dr  = tabs[i]; }
-        else if (curURL.includes('prdtfs.uticorp.com')) { qaData.tabs.tfs = tabs[i]; }
-        else if (curURL.includes('uti.blackboard.com')) { qaData.tabs.bb  = tabs[i]; }
+//test data to send to content.js
+var bob = {
+      message : "Test Message",
+      colors : {
+        green : [
+          {
+            width : 315,
+            height : 32,
+            left : 300,
+            top : 50
+          },
+          {
+            width : 100,
+            height : 100,
+            left : 50,
+            top : 50
+          },
+          {
+            width : 100,
+            height : 100,
+            left : 150,
+            top : 150
+          }
+        ],
+        red : [
+          {
+            width : 100,
+            height : 100,
+            left : 300,
+            top : 300
+          },
+          {
+            width : 100,
+            height : 100,
+            left : 500,
+            top : 500
+          },
+          {
+            width : 100,
+            height : 100,
+            left : 700,
+            top : 700
+          }
+        ]
       }
-    });
+    };
+
+  console.log("initialize");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tab_info) {
+    console.log(tab_info[0].url);
+    //provide different pages for different websites when users click on the extension
+    if(tab_info[0].url.includes('avondale-iol')){
+      //load specific helper bar on the page (you can't get rid of the popup once you set it)
+      chrome.tabs.sendMessage(tab_info[0].id, bob, function(response) {});
+      console.log('avon', response);
+    } else if(tab_info[0].url.includes('prdtfs.uticorp.com')){
+      //load specific helper bar on the page (you can't get rid of the popup once you set it)
+      chrome.tabs.sendMessage(tab_info[0].id, bob, function(response) {});
+      console.log('tfs');
+    } else if(tab_info[0].url.includes('uti.blackboard.com')){
+      //load specific helper bar on the page (you can't get rid of the popup easily once you set it)
+      chrome.tabs.sendMessage(tab_info[0].id, bob, function(response) {});
+      console.log('bb');
+    } else { //open the 3 needed pages for tfs
+      qaToolIsActive = true;
+
+      chrome.windows.create({ 
+        url: [
+          'http://avondale-iol/AD-105.html',
+          'https://prdtfs.uticorp.com/UTI-ALM/IT/BMS/_backlogs?level=Projects&showParents=false&_a=backlog',
+          'https://uti.blackboard.com/webapps/login/?action=relogin'
+        ]
+      },
+
+      // callback after the window is created
+      function(win) {
+        qaData.window = win;
+
+        // cycle through the tabs in the new window
+        chrome.tabs.query({ windowId : qaData.window.id },
+        function (tabs) {
+          for (var i = 0; i < tabs.length; i++) {
+            var curURL = tabs[i].url;
+
+            if (curURL.includes('avondale-iol'))            { qaData.tabs.dr  = tabs[i]; }
+            else if (curURL.includes('prdtfs.uticorp.com')) { qaData.tabs.tfs = tabs[i]; }
+            else if (curURL.includes('uti.blackboard.com')) { qaData.tabs.bb  = tabs[i]; }
+          }
+          // console.log(win);
+          // console.log(tabs);
+        });
+      });
+    }
   });
 }
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-  // UI event listeners
-  document.getElementById('initialize').addEventListener('click', init);
+//   // UI event listeners
+//   document.getElementById('initialize').addEventListener('click', init);
 
-})
+// })
 
-function init() {
+
   chrome.runtime.sendMessage({ 'message' : 'initialize' });
-}
+  
