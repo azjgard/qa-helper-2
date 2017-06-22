0aba0b1eaa6cd10a5129f649a8791b1ef64d5d97
Jordin Gardner <jgardner@uti.edu>
2017-06-22 11:11:25 -0700

    deleted unnecessary files and created nested folder structure

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // UI event listeners
-  document.getElementById('initialize').addEventListener('click', init);
-  document.getElementById('run').addEventListener('click', run)
-})
-
-function run() {
-  chrome.runtime.sendMessage({ 'message' : 'run' });
-}
