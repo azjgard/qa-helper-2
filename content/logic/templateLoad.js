40e66002a4d6f3f4d72462ef4f14afd6d82812e7
Jordin Gardner <jgardner@uti.edu>
2017-06-26 10:29:18 -0700

    ui templates also contain information for event listeners now

--- content/logic/templateLoad.js
+++ content/logic/templateLoad.js
@@ -1,22 +1,31 @@
-var templateObjects = {
+function thisIsARandomFunction() {
+  alert('HELLO THERE WORLD!');
+}
+function anotherThing() {
+  alert('this is another thing');
+}
 
+// TODO: add functionality to account for hotkeys
+var templateObjects = {
   "tfs": {
     title   : 'Team Foundation Server',
     buttons : [
       {
         text    : 'Jump to Kanban',
-        hotkey  : 'Ctrl+Shift+S'
+        hotkey  : 'Ctrl+Shift+S',
+        id: 'qa-ext_jump-to-kanban',
+        listener: anotherThing
       },
       {
         text    : 'Settings',
         classes : ['fee', 'fie', 'fo', 'fum'],
         id      : 'qa-ext_settings',
-        hotkey  : 'Ctrl+Shift+S'
+        hotkey  : 'Ctrl+Shift+S',
+        listener: thisIsARandomFunction
       }
     ],
     showCloseButton: true
   },
-
   "old-slide": {
     title : 'Old Slide',
     buttons : [
@@ -31,6 +40,7 @@ var templateObjects = {
         hotkey : 'none'
       },
     ],
+    listeners : [],
     showCloseButton: false
   },
 
@@ -48,6 +58,7 @@ var templateObjects = {
         hotkey : 'none'
       },
     ],
+    listeners : [],
     showCloseButton: false
   },
 
@@ -58,6 +69,7 @@ var templateObjects = {
         text : 'No buttons here',
       }
     ],
+    listeners : [],
     showCloseButton: true
   },
 
@@ -73,23 +85,47 @@ var templateObjects = {
         hotkey : 'none'
       },
     ],
+    listeners : [],
     showCloseButton: true
   },
 
 }
 
 function loadTemplate(context) {
-  var template = null;
+  return new Promise(function(resolve, reject) {
+    var template = null;
+    var templateUT = null;
 
-  // don't load a template if it's a miscellaneous page
-  if (context !== 'misc') {
-    template = templateObjects[context];
-    template = generateTemplate(template);
+    // don't load a template if it's a miscellaneous page
+    if (context !== 'misc') {
+      template = templateObjects[context];
+      templateUI = generateTemplate(template);
 
-    $('body').append('<div class="null-container">')
-             .append(template)
-             .append('</div>');
+      // add the UI to the document
+      $('body').append('<div class="null-container">')
+               .append(templateUI)
+               .append('</div>');
 
-    $('.draggable').draggable({ handle: '.grabbable' });
-  }
+      $('.draggable').draggable({ handle: '.grabbable' });
+
+      // add the listeners to the buttons
+      for (var i = 0; i < template.buttons.length; i++) {
+        var btn = template.buttons[i];
+
+        if (btn.id && btn.listener) {
+          $('#' + btn.id).on('click', btn.listener);
+        }
+        else {
+          if (!btn.id) {
+            throw new Error("The button " + btn.text + " has no ID to reference it by!");
+          }
+          if (!btn.listener) {
+            throw new Error("The button " + btn.text + " has no listener function attached!");
+          }
+        }
+      }
+    }
+
+    resolve(context);
+  });
 }
