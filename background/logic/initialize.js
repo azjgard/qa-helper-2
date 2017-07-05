b254e8d0454232d93de1c4b9944b896721268d37
Jordin Gardner <jgardner@uti.edu>
2017-07-05 12:54:00 -0700

    fixed width and height issues

--- background/logic/initialize.js
+++ background/logic/initialize.js
@@ -15,20 +15,27 @@ function initializeQaTool() {
     function generateTabs(hasMultipleDisplays, displayData) {
       if (hasMultipleDisplays) {
         var monitor1 = displayData[0];
+        var monitor2 = displayData[0];
 
         chrome.windows.create({
+          height: monitor1.bounds.height,
+          width: monitor1.bounds.width,
           left: 0,
+          top: 0,
           url: [
-            'https://uti.blackboard.com/webapps/login/?action=relogin', //blackboard
-            'http://avondale-iol/index.html' //old site
+            'https://uti.blackboard.com/webapps/login/?action=relogin' //blackboard
           ]
         });
 
         chrome.windows.create({
-          left: monitor1.bounds.width + 1,
+          height: monitor2.bounds.height,
+          width: monitor2.bounds.width,
+          left: monitor1.bounds.width,
+          top: 0,
           url: [
           'https://prdtfs.uticorp.com/UTI-ALM/IT/BMS/_backlogs?level=Stories&showParents=true&_a=backlog', //tfs backlog
-          'https://prdtfs.uticorp.com/UTI-ALM/IT/BMS/_backlogs/board/Features?scrollToQA=true' //tfs board
+          'https://prdtfs.uticorp.com/UTI-ALM/IT/BMS/_backlogs/board/Features?scrollToQA=true', //tfs board
+          'http://avondale-iol/' // DR site
           ]
         })
       }
